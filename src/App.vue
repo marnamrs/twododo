f085e63c55d85979dc98993f988be5dcb28d1413
Marina M <marnacode@gmail.com>
2022-12-12 13:11:01 +0100

    add pre-setup

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+        <nav>
+                <RouterLink to="/">Home</RouterLink>
+
+
+        </nav>
+
+
+        <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+        this is the homeview page
+</template>
