605e73c6c8122b33dc402cfda1fd7d8b5d294a6c
Marina M <marnacode@gmail.com>
2022-12-13 16:54:39 +0100

    set login redirect fix pinia persist

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import { createApp, markRaw } from "vue";
 import { createPinia } from "pinia";
+import piniaPersist from "pinia-plugin-persist";
 
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
 
 const app = createApp(App);
-const pinia = createPinia();
-pinia.use(({ store }) => {
-        store.$router = markRaw(router);
-});
+const pinia = createPinia()
+        .use(piniaPersist)
+        .use(({ store }) => {
+                store.$router = markRaw(router);
+        });
 
 app.use(pinia);
 app.use(router);
